613430bd4cea699ca4a275b0f303be581a4ddb24
JacksonMartin <jake.gardin.martin@gmail.com>
2017-04-27 18:44:05 -0400

    Add files via upload

---
+++ length.py
@@ -0,0 +1,12 @@
+import nltk
+
+class LengthRule():
+    def __init__(self, second, first, zero):
+        self.second = second
+        self.first = first
+        self.zero = zero
+    
+    def eval(self, string):
+        return self.second * (len(string) ** 2) + self.first * len(string) + self.zero
+    
+    def mutate(self,
