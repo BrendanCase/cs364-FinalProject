7d7f80a3c016a0a34a6ffe44f4178d77aa87dd2f
Brendan Case <bcase@oberlin.edu>
2017-04-27 20:24:42 -0400

    forgot one of the necessary files

--- hellogram.py
+++ hellogram.py
@@ -7,7 +7,7 @@ Adjectives = set([word for (word, tag) in brown.tagged_words(tagset='universal')
 Pronouns = set([word for (word, tag) in brown.tagged_words(tagset='universal') if tag == 'PRON'])
 Determiners = set([word for (word, tag) in brown.tagged_words(tagset='universal') if tag == 'DET'])
 Nouns = set([word for (word, tag) in brown.tagged_words(tagset='universal') if tag == 'NOUN'])
-IntransitiveVerbs = set([w.strip('\n') for w in open('la')])
+IntransitiveVerbs = set([w.strip('\n') for w in open('intransitives.txt')])
 Verbs = set([word for (word, tag) in brown.tagged_words(tagset='universal') if tag == 'VERB'])
 
 def main():
@@ -15,15 +15,15 @@ def main():
         S -> NP VP [1.0]
         NP -> Nom [0.7] | | Det Nom [0.2] | Pronoun [0.1]
         Nom -> Adj Nom [0.2] | Noun Nom [0.3] | Noun [0.3] | Nom PP [0.2]
-    PP -> Prep NP [1.0]
-    Prep -> '*' [1.0]
-    Adj -> '*' [1.0]
-    Pronoun -> '*' [1.0]
-    Det -> '*' [1.0]
-    Noun -> '*' [1.0]
-    VP -> IV [0.3] | TV NP [0.3] | TV NP PP [0.2] | TV PP [0.2]
-    IV -> '*' [1.0]
-    TV -> '*' [1.0]""")
+        PP -> Prep NP [1.0]
+        Prep -> '*' [1.0]
+        Adj -> '*' [1.0]
+        Pronoun -> '*' [1.0]
+        Det -> '*' [1.0]
+        Noun -> '*' [1.0]
+        VP -> IV [0.3] | TV NP [0.3] | TV NP PP [0.2] | TV PP [0.2]
+        IV -> '*' [1.0]
+        TV -> '*' [1.0]""")
 
     test.addTerminals('Prep', ['on', 'from', 'to'])
     test.addTerminals('Adj', sample(Adjectives, 15))
