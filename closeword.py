c6ba4d43c1901cb81172e4a39cad7ae01f8343f0
Joe Martin <jmartin@oberlin.edu>
2017-04-27 21:05:28 -0400

    for wrodrule added a mutator

--- closeword.py
+++ closeword.py
@@ -5,22 +5,30 @@ class WordRule(metric.Metric):
         self.goodWords = goodWords
         self.badWords = badWords
 
-    def getLCHDepth(syns1, syns2):
+    def getLCHDepth(self, syns1, syns2):
         lch = csyn.lowest_common_hypernyms(syns2)
         minDepth = wn.synset(lch[0].name()).min_depth()
 
     def getSimilarity(self, word1, word2):
         w1syns = wn.synsets(word1)
         w2syns = wn.synsets(word2)
-        depths = [getLCHDepth(w1syn, w2syn) for w1syn in w1syns for w2syn in w2syns])
+        depths = [self.getLCHDepth(w1syn, w2syn) for w1syn in w1syns for w2syn in w2syns])
         return max(depths)
         
-    def getRating(word, wordList):
-        return max([getSimilarity(word, oWord) for oWord in wordList])
+    def getRating(self, word, wordList):
+        return max([self.getSimilarity(word, oWord) for oWord in wordList])
 
-    def getSentanceScore(sentance, wordList):
+    def getSentanceScore(self, sentance, wordList):
         sentanceWords = sentance.split(' ')
-        return sum([getRating(word, wordList) for word in sentanceWords])
+        return sum([self.getRating(word, wordList) for word in sentanceWords])
 
-    def evaluate(string):
-        return getSentanceScore(string, self.goodWords) - getSentanceScore(string, self.badWords)
+    def evaluate(self, string):
+        return self.getSentanceScore(string, self.goodWords) - self.getSentanceScore(string, self.badWords)
+
+    def mutate(self, string, value):
+        words = string.split(' ')
+        if value < 0:
+            self.badWords = self.badWords | set(words)
+        elif value > 0:
+            self.goodWords = self.goodWords | set(words)
+
