30137bba41ae1abde92428322d784559aaf8eddd
Jackson Martin <jake.gardin.martin@gmail.com>
2017-04-30 16:09:42 -0400

    mutator?

--- mutator.py
+++ mutator.py
@@ -8,23 +8,15 @@ def mutateWeights(grammar):
     leftHandSides = [prod._lhs for prod in grammar._productions]
     leftHandSide = random.choice(leftHandSides)
     productions = grammar.productions(lhs=leftHandSide)
-    otherProductions = [prod for prod in grammar.productions() if prod not in productions]
-    newProbs = np.random.dirichlet(np.ones(len(productions)),size=1)[0]
-    for i, newProb in enumerate(newProbs):
-        prod = productions[i]
-        lhs = prod._lhs
-        rhs = prod._rhs
-        newProd = nltk.ProbabilisticProduction(lhs,rhs,prob=newProb)
-        otherProductions.append(newProd)
-    newGrammar = nltk.PCFG(start, otherProductions)
-    return newGrammar
-
-    list #this is the list of probabilities
+    otherProductions = [prod for prod in grammar.productions() if prod not in productions]    
     lower = 0
     upper = 1
-    current value
     standarddeviationsayswhat = .2
-    trunk = scipy.stats.truncnorm.rvs((lower - current value)/standarddeviationsayswhat, (upper - current value)/standarddeviationsayswhat, loc=current value, scale = standarddeviationsayswhat)
-    sum = sum(list)
-    for v in list:
-        v = v/sum
+    p1 = random.choice(productions)
+    p1.prob = scipy.stats.truncnorm.rvs((lower - p1.prob)/standarddeviationsayswhat, (upper - p1.prob)/standarddeviationsayswhat, loc=p1.prob, scale = standarddeviationsayswhat)
+    sum = sum([p.prob for p in productions])
+    for p in productions:
+        p.prob = p.prob/sum 
+    otherProductions.append(productions)
+    newGrammar = nltk.PCFG(start, otherProductions)
+    return newGrammar
