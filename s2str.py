99b173de5f8ccfbe8621f69c80613d30805424e2
Brendan Case <bcase@oberlin.edu>
2017-05-04 23:20:40 -0400

    hei

--- s2str.py
+++ s2str.py
@@ -4,37 +4,26 @@ an illustration of initializing a producer with a grammar based
 on species2.pcfg and making some posts
 '''
 
-import nltk
-from nltk.corpus import gutenberg
-from random import sample
 from producer_class import addterminals
-from producer_class import Producer
 
-#def main():
 def getGString():
-    rules = [
-        ('Det', ['the', 'this', 'that', 'each', 'every', 'another']),
-        ('Ord', ['first', 'second', 'last', 'middle', 'third', 'final', 'penultimate']),
-        ('Adj', ['green', 'blue', 'winning', 'cheerful', 'elated']),
-        ('SingNoun', ['duck', 'ant', 'egg', 'window', 'cereal', 'AI project']),
-        ('SingPropNoun', ['Jack', 'Jill', 'Fred', 'George']),
-        ('Prep', ['on', 'for', 'from', 'by', 'with']),
-        ('AdvPlace', ['here', 'there', 'everywhere']),
-        ('AdvTimePres', ['today', 'Friday', 'this week', 'sometime']),
-        ('AdvTimePast', ['yesterday', 'last Wednesday', 'last month'])
-    ]
+    wordbank = {
+        'Det' : ['the', 'this', 'that', 'each', 'every', 'another'],
+        'Ord' : ['first', 'second', 'last', 'middle', 'third', 'final', 'penultimate'],
+        'Adj' : ['green', 'blue', 'winning', 'cheerful', 'elated'],
+        'SingNoun' : ['duck', 'ant', 'egg', 'window', 'cereal', 'AI project'],
+        'SingPropNoun' : ['Jack', 'Jill', 'Fred', 'George'],
+        'Prep' : ['on', 'for', 'from', 'by', 'with'],
+        'AdvPlace' : ['here', 'there', 'everywhere'],
+        'AdvTimePres' : ['today', 'Friday', 'this week', 'sometime'],
+        'AdvTimePast' : ['yesterday', 'last Wednesday', 'last month']
+    }
 
     skeleton_gram_str = ''.join(open('species2.pcfg'))
 
-    for r in rules:
-        skeleton_gram_str = addterminals(skeleton_gram_str, r[0], r[1])
+    for w in wordbank.keys():
+        skeleton_gram_str = addterminals(skeleton_gram_str, w, wordbank[w])
 
-    #prod = Producer('/u/_junebug_', skeleton_gram_str)
+    return skeleton_gram_str, wordbank
 
-    #for i in range(10):
-    #    print(prod.parent_grammar.get_post())
-    return skeleton_gram_str
-
-#if __name__ == '__main__':
-#    main()
 
