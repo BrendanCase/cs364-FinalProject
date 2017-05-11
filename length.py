ec6d8769380b1ddca9f05c96eabcea6a76b96a3f
Jackson Martin <jake.gardin.martin@gmail.com>
2017-05-10 21:46:33 -0400

    makes length less likely to mutate

--- length.py
+++ length.py
@@ -15,15 +15,15 @@ class LengthRule(metric.Metric):
         return 4 * self.second * (len(string) - self.first) * (len(string) - self.zero) / ((self.first - self.zero) (self.zero - self.first))
         
     def mutate(self, bull, change):
-        if rand.random() > .5:
+        if rand.random() > .95:
             self.second = self.second - change
             if self.second == 0:
                 self.second = 1
-        if rand.random() >.5:
+        if rand.random() >.95:
             self.first = self.first - change
             if self.first == self.zero:
                 self.first += 1
-        if rand.random() > .5:
+        if rand.random() > .95:
             self.zero = self.zero - change
             if self.zero == self.first:
                 self.zero -= 1
